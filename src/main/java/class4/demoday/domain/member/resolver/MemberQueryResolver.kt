2a104622dd3affde671fe65a50c94959c85b07c0
snowykte0426 <snowykte0426@naver.com>
2024-10-21 13:48:42 +0900

    feat: 계정조회 로직 구현

--- src/main/java/class4/demoday/domain/member/resolver/MemberQueryResolver.kt
+++ src/main/java/class4/demoday/domain/member/resolver/MemberQueryResolver.kt
@@ -3,12 +3,15 @@ package class4.demoday.domain.member.resolver
 
 import class4.demoday.domain.member.service.MemberInquiryService
 import class4.demoday.global.member.entity.Member
+import class4.demoday.global.security.roles.Roles
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class MemberQueryResolver(private val memberInquiryService: MemberInquiryService
+class MemberQueryResolver(
+    private val memberInquiryService: MemberInquiryService
 ) {
 
     @QueryMapping
